c2f44df5620905c0435e7464031c0bdf2eff6dee
Ania\ Leśniak <anna.lesniak@llp.pl>
2010-09-29 17:23:21 +0200

    Added ActionController::RoutingError as default error

--- lib/vidibus-routing_error.rb
+++ lib/vidibus-routing_error.rb
@@ -2,6 +2,8 @@ $:.unshift(File.join(File.dirname(__FILE__), "..", "lib", "vidibus"))
 require "routing_error/rack"
 
 module Vidibus::RoutingError
+  ERRORS = [ActionController::RoutingError]
+  
   class Engine < ::Rails::Engine
     unless Rails.env.test?
       config.app_middleware.insert_before("ActionDispatch::ShowExceptions", "Vidibus::RoutingError::Rack")
